6bb40b610d0ea4aa0fce3ad42e7ed7e8e0b384f5
takamuraTK <tirpitz.zzz@outlook.jp>
2020-02-25 11:37:07 +0900

    s3ファイル削除の例の修正

--- serverless/services/test-lambda/handler.py
+++ serverless/services/test-lambda/handler.py
@@ -1,33 +1,6 @@
 import boto3
 
-bucket = "s3-delete-file"
-prefix = "test_folder/folder/" #削除対象のファイルがあるディレクトリ
-s3client = boto3.client('s3')
-delete_extension = ".cache" #削除対象の拡張子
+bucket = "s3-delete"
+prefix = "test_folder/folder/"
 
 def delete(event, context):
-  next_token = ''
-
-  while True: #breakになるまで処理が続く。
-    if next_token == '': # オブジェクト件数が1000個以上あった場合を想定
-      response = s3client.list_objects_v2(Bucket=bucket, Prefix=prefix, StartAfter=prefix)
-    else:
-      response = s3client.list_objects_v2(Bucket=bucket, Prefix=prefix, StartAfter=prefix, ContinuationToken=next_token)
-    # StartAfterに指定したそのキーよりも後のオブジェクトを取得する
-
-    if 'Contents' in response:
-      contents = response['Contents']
-
-    for content in contents:
-      filedir = content['Key']
-      filename = filedir.replace(prefix, '')
-      if delete_extension in filename:
-        print(filename + "を削除します")
-        s3client.delete_object(Bucket=bucket, Key=filedir)
-      else:
-        print(filename + "は削除しません") #確認のためのメッセージ、elseごとなくていい
-
-    if 'NextContinuationToken' in response: # responseにオブジェクト件数が1000個以上あるときに発行されるやつ
-      next_token = response['NextContinuationToken']
-    else:
-      break
